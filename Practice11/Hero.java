90c148dea7c3c88043acda47c74635653e6d794f
Maddy0906 <madu121@me.com>
2017-05-08 17:49:42 -0700

    Practice11/ 5 May 2017

--- Practice11/Hero.java
+++ Practice11/Hero.java
@@ -4,4 +4,18 @@ package Java.Practice11;
  * Created by madoka on 2017/05/08.
  */
 public class Hero {
+    private String name = "minato";
+    private int hp = 100;
+
+    //fight
+    public void attack(Matango m){
+        System.out.println(this.name + "attack!");
+        m.hp -=5;
+        System.out.println("5point damage to enemy");
+    }
+
+    //escape
+    public void run(){
+        System.out.println(this.name+ "run away!");
+    }
 }
--- Practice11/practice/Matango.java
+++ Practice11/practice/Matango.java
@@ -1,7 +1,20 @@
 package Java.Practice11.practice;
 
+import Java.Practice11.Hero;
+
 /**
  * Created by madoka on 2017/05/08.
  */
 public class Matango {
+    int hp = 50;
+    private char suffix;
+    public Matango(char suffix){
+        this.suffix = suffix;
+    }
+
+    public void attack(Hero h){
+        System.out.println("Mashroom"+ this.suffix + "attack");
+        System.out.println("10 damages");
+        h.setHp(h.getHp()-10);
+    }
 }
